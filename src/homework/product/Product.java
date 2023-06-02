a372005959bcd55a06b0eb9e4228fe8f4683636f
OroszSzilard <oroszszilard05@gmail.com>
2023-06-02 17:36:18 +0200

    Update Product.java

--- src/homework/product/Product.java
+++ src/homework/product/Product.java
@@ -1,35 +1,42 @@
 package homework.product;
 
-public class Product {
-    private String name;
-    private int price;
+public class Product 
+{
+	protected String name;
+	protected int price;
+	public Product(String name, int price) {
+		super();
+		this.name = name;
+		if(price>=0)
+		this.price = price;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		if(price>=0)
+		this.price = price;
+	}
+	public void decreasePrice(int percent)
+	{
+		price = price-(Math.round(((float)price/100)*percent));
+	}
+	
+	public void increasePrice(int percenatge) {
+		if(percenatge > 0) {
+			price += Math.round(price*(float)percenatge/100);
+		}		
+	}
+	
+	@Override
+	public String toString() {
+		return "Product [name=" + name + ", price=" + price + "]";
+	}
 
-    public Product(String name, int price) {
-        this.name = name;
-        setPrice(price);
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public int getPrice() {
-        return price;
-    }
-
-    public void setPrice(int price) {
-        if (price >= 0) {
-            this.price = price;
-        }
-    }
-
-    public void decreasePrice(int percent) {
-        double discount = price * (percent / 100.0);
-        price -= discount;
-    }
-
-    @Override
-    public String toString() {
-        return "Product: " + name + ", Price: " + price;
-    }
 }
